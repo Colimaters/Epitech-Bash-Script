6653ca8a0f30d68842c4801ecf95f00c98e78922
Kenshiro <47219159+Colimaters@users.noreply.github.com>
2019-05-04 16:49:50 +0200

    update for save time

--- my_clone.sh
+++ my_clone.sh
@@ -2,13 +2,15 @@
 
 clear
 echo -e "Ok commençons à cloner !\n"
-read -p "QuI est le propriétaire du repo (login) ? " login
+read -p "Qui est le propriétaire du repo (login sans @) ? " login
 read -p "Et le nom du repo ? " nomrepo
 
-if [ $login = moi ] || [ $login = darius.morel@epitech.eu ]; then
+if [ $login = moi ] || [ $login = darius.morel ]; then
     echo -e "C'est parti pour cloner un repo perso !\n"
     git clone git@git.epitech.eu:/darius.morel@epitech.eu/$nomrepo
 else
     echo -e "Clonons un repo d'un copain !\n"
-    git clone git@git.epitech.eu:/$login/$nomrepo
+    git clone git@git.epitech.eu:/$login@epitech.eu/$nomrepo
 fi
+
+echo -e "Fin du clonage !"
