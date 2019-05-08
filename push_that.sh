0cb22a4a3c33b5eaf266e98814584b2ba9252d4f
Kenshiro <47219159+Colimaters@users.noreply.github.com>
2019-05-08 14:59:49 +0200

    handling no arguments, add verbose mode (with -v)

--- push_that.sh
+++ push_that.sh
@@ -4,6 +4,22 @@ commit=$*
 
 clear
 
-git add --all
-git commit -m "$commit"
-git push
+if [ $# = 0 ]; then
+    echo -e "Enter arguments for quick commit message or -v to verbose mode !"
+    exit 84
+elif [ $# = 1 ] && [ $1 = "-v" ]; then
+    echo -e "Add all files !\n"
+    git add --all
+    read -p "Enter your commit message : " commit
+    echo
+    git commit -m "$commit"
+    if [ $(echo $?) = 1 ]; then
+        exit 84
+    fi
+    git push
+    echo -e "\nPush files to your repo !\n"
+else
+    git add --all
+    git commit -m "$commit"
+    git push
+fi
