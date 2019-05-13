43c3fd724e57ea926511e3f1711b4a0b0dc019ee
Kenshiro <darius.morel@epitech.eu>
2019-05-13 15:54:20 +0200

    Less line and opti code
    Add the loop function that handle argument
    less the loop for view the ACL

--- prepare_my_repo.sh
+++ prepare_my_repo.sh
@@ -8,18 +8,20 @@ if [ $# != 0 ]; then
     nomrepo=$1
 else
     echo -e "Il faut mettre un nom de repo sinon ça marche pas"
-    exit 0
+    exit 84
 fi
 
-#loop ()
-#{
-#    a=$(echo $?)
-#    while [ $a != 1 ]; do
-#        echo -e "Mauvais mot de passe"
-#        `$*`
-#        a=$(echo $?)
-#    done
-#}
+loop ()
+{
+    while [ $(echo $?) = 1 ]; do
+        echo -e "Mauvais mot de passe"
+        if [ $# = 1 ]; then
+            blih -u darius.morel@epitech.eu repository "$1" "$nomrepo"
+        else
+            blih -u darius.morel@epitech.eu repository "$1" "$nomrepo" "$2" "$3"
+        fi
+    done
+}
 
 clear
 echo -e "Créons un repo tous ensemble !\n"
@@ -27,25 +29,18 @@ echo -e "Créons un repo tous ensemble !\n"
 
 echo -e "Création du repo : Tape ton mot de passe ! "
 blih -u darius.morel@epitech.eu repository create $nomrepo
-while [ $(echo $?) = 1 ]; do
-        echo -e "Mauvais mot de passe"
-        blih -u darius.morel@epitech.eu repository create "$nomrepo"
-done
-#loop "blih -u darius.morel@epitech.eu repository create $nomrepo"
+loop "create"
 
 
 echo -e  "\nPermission à Tek : Retape ton mot de passe ! "
 blih -u darius.morel@epitech.eu repository setacl "$nomrepo" ramassage-tek r
-while [ $(echo $?) = 1 ]; do
-        echo -e "Mauvais mot de passe"
-        blih -u darius.morel@epitech.eu repository setacl "$nomrepo" ramassage-tek r
-done
-#loop blih -u darius.morel@epitech.eu repository setacl "$nomrepo" ramassage-tek r
+loop "setacl" "ramassage-tek" "r"
 
 read -p "Une autre permission ? " right
 while [ $right != "non" ]; do
-    echo -e "Permission accordée à : " "$right"
     blih -u darius.morel@epitech.eu repository setacl "$nomrepo" "$right" rw
+    loop "setacl" "$right" "rw"
+    echo -e "Permission accordée à : " "$right"
     read -p "Une autre permission ? " right
 done
 
@@ -53,16 +48,10 @@ done
 echo -e  "\nRepo créé le " && date "+%d/%m/%Y"
 echo "     à"
 date "+%Hh%Mm et %Ss"
-echo -e "\n"
 
 
 echo "Affichage des permissions : Tape ton mot de passe ! "
 blih -u darius.morel@epitech.eu repository getacl "$nomrepo"
-while [ $(echo $?) = 1 ]; do
-        echo -e "Mauvais mot de passe"
-        blih -u darius.morel@epitech.eu repository getacl "$nomrepo"
-done
-#loop blih -u darius.morel@epitech.eu repository getacl "$nomrepo"
 
 git clone git@git.epitech.eu:/darius.morel@epitech.eu/"$nomrepo"
 
