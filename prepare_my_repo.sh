9140b724f5a365d20ea9429ae0d0d6846033d4b4
Kenshiro <47219159+Colimaters@users.noreply.github.com>
2020-03-02 00:02:00 +0100

    Clearer messages and no bugs

--- prepare_my_repo.sh
+++ prepare_my_repo.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-pbase='~/base/norm/*'
-pmath='~/base/math/*'
-psfml='~/base/csfml/*'
-
 if [ $# != 0 ]; then
     nomrepo=$1
 else
@@ -26,48 +22,48 @@ loop ()
 clear
 echo -e "Créons un repo tous ensemble !\n"
 
-
 echo -e "Création du repo : Tape ton mot de passe ! "
 blih -u darius.morel@epitech.eu repository create $nomrepo
 loop "create"
-
 
 echo -e  "\nPermission à Tek : Retape ton mot de passe ! "
 blih -u darius.morel@epitech.eu repository setacl "$nomrepo" ramassage-tek r
 loop "setacl" "ramassage-tek" "r"
 
-read -p "Une autre permission ? " right
+echo
+read -p "Une autre permission (login sans @) / (non) ? " right
+
 while [ $right != "non" ]; do
+    right=$right@epitech.eu
     blih -u darius.morel@epitech.eu repository setacl "$nomrepo" "$right" rw
     loop "setacl" "$right" "rw"
-    echo -e "Permission accordée à : " "$right"
-    read -p "Une autre permission ? " right
+    echo -e "Permission accordée à : $right\n"
+    read -p "Une autre permission (login sans @) / (non) ? " right
 done
 
-
-echo -e  "\nRepo créé le " && date "+%d/%m/%Y"
-echo "     à"
-date "+%Hh%Mm et %Ss"
-
-
-echo "Affichage des permissions : Tape ton mot de passe ! "
+echo -e "\nAffichage des permissions : Tape ton mot de passe ! "
 blih -u darius.morel@epitech.eu repository getacl "$nomrepo"
+echo
 
 git clone git@git.epitech.eu:/darius.morel@epitech.eu/"$nomrepo"
+echo
 
-read -p "Quel type de repo dois-je faire ? " type
+read -p "Quel type de repo dois-je faire (base) / (math) / (csfml) ? " type
 
-if [ $type = "base" ]; then
-    cp -r $pbase $nomrepo
-    echo -e "Copie de la base dans le repo"
+if [ -z $type ]; then
+    echo -e "Ok rien de spécial"
+elif [ $type = "base" ]; then
+    cp -r ~/base/norm $nomrepo
+    echo -e "Copie de la base C dans le repo"
 elif [ $type = "math" ]; then
-    cp -r $pmath $nomrepo
+    cp -r ~/base/math $nomrepo
     echo -e "Copie de la base des math"
 elif [ $type = "csfml" ]; then
-    cp -r $psfml $nomrepo
+    cp -r ~/base/csfml $nomrepo
     echo -e "Copie de la base de la CSFML"
 else
-    echo -e "Ok rien de spécial"
+    echo -e "Type inconnue !"
 fi
+echo
 read -p "Press enter to clear !" wait
 clear
